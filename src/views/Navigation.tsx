27a2bafb4bf7de162cdde37e4b9948421ea1b560
vitalik140220 <79133072+vitalik140220@users.noreply.github.com>
2022-08-15 10:17:53 +0300

    Fix authRoutes

--- src/views/Navigation.tsx
+++ src/views/Navigation.tsx
@@ -1,52 +1,8 @@
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React, {FC, useEffect} from 'react';
-import Confirm from './Confirm';
-import Main from './Main';
-import SignIn from './SignIn';
-import SignUp from './SignUp';
-import {useState} from 'react';
-import {Auth} from 'aws-amplify';
+import AuthRoutes from '../routes/AuthRoutes';
 
 const Navigation: FC = () => {
-  const Stack = createNativeStackNavigator();
-
-  const [user, setUser] = useState(null);
-
-  const checkUser = async () => {
-    try {
-      const authUser = await Auth.currentAuthenticatedUser({bypassCache: true});
-      console.log('hello');
-      setUser(authUser);
-    } catch (e) {
-      setUser(null);
-    }
-  };
-
-  useEffect(() => {
-    checkUser();
-  }, []);
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {user ? (
-          <Stack.Screen
-            name="main"
-            component={() => <Main  />}
-          />
-        ) : (
-          <>
-            <Stack.Screen
-              name="signIn"
-              component={() => <SignIn />}
-            />
-            <Stack.Screen name="signUp" component={SignUp} />
-            <Stack.Screen name="confirm" component={Confirm} />
-          </>
-        )}
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  return <AuthRoutes />;
 };
 
 export default Navigation;
