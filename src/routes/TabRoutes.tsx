44914baf47adaec05a2a8a9deaa675efa3c88443
vitalik140220 <79133072+vitalik140220@users.noreply.github.com>
2022-08-22 18:52:38 +0300

    Add style tab

--- src/routes/TabRoutes.tsx
+++ src/routes/TabRoutes.tsx
@@ -1,5 +1,6 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import React, {FC} from 'react';
+import {TouchableOpacity, View} from 'react-native';
 import Main from '../views/Main';
 import Settings from '../views/Settings';
 import Transactions from '../views/Transactions';
@@ -7,7 +8,22 @@ import MainRoutes from './MainRoutes';
 const Tab = createBottomTabNavigator();
 const TabRoutes: FC = () => {
   return (
-    <Tab.Navigator screenOptions={{headerShown: false}}>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          position: 'absolute',
+          bottom: 20,
+          left: 30,
+          right: 30,
+          borderRadius: 15,
+          height: 70,
+          backgroundColor: 'rgba(182, 182, 182, 1)',
+        },
+        tabBarButton: props => {
+          return <TouchableOpacity {...props} delayPressIn={0} />;
+        },
+      }}>
       <Tab.Screen name="main" component={MainRoutes} />
       <Tab.Screen name="transactions" component={Transactions} />
       <Tab.Screen name="settings" component={Settings} />
